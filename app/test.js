e5b716f4cca0710aa5f4b6f7852bfe35c012ad99
Mahmoud Tarek <mahmoud.tarek3500@gmail.com>
2026-09-11 20:40:35 +0300

    Add automated tests

---
+++ app/test.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const assert = require("assert");
+
+const server = require("./server");
+
+http.get("http://localhost:3000", (res) => {
+  let data = "";
+
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  res.on("end", () => {
+    assert.strictEqual(res.statusCode, 200);
+    assert.strictEqual(data, "CI/CD Project is Working!\n");
+
+    console.log("Test Passed!");
+    server.close();
+  });
+});
